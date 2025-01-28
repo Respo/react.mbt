43eac776aa491f952f572ddc33ae5f38f6cad3a3
tiye <jiyinyiyong@gmail.com>
2025-01-29 01:34:49 +0800

    attempt for use_state

--- src/preact.mjs
+++ src/preact.mjs
@@ -1,5 +1,6 @@
 import * as Preact from "preact";
-import * as PreactHooks from "preact/hooks";
+import * as PreactHooks from "../node_modules/preact/hooks/src/index.js";
+console.log(PreactHooks);
 
 window.Preact = Preact;
 window.PreactHooks = PreactHooks;
